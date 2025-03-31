db6cef19b5793d4cfae4922ce814c8de42b834ff
tokryr <topias.yrjola@gmail.com>
2025-03-31 16:05:10 +0300

    remove unnecessary loader

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,9 @@
 import { createRoute } from '@tanstack/react-router';
 import { Route as RootRoute } from './__root';
 import WelcomePage from '@/pages/WelcomePage';
-import { getTasks } from '@/services/taskService';
 
 export const Route = createRoute({
   getParentRoute: () => RootRoute,
   path: '/',
-  loader: async () => {
-    const tasks = await getTasks();
-    return { tasks };
-  },
   component: WelcomePage,
 });
